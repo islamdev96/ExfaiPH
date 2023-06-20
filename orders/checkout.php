6f7fe5b5de764789159c13f2f226f119574d8211
islamdev96 <islamsayedglab@gmail.com>
2023-06-20 23:10:28 +0300

    ..

--- orders/checkout.php
+++ orders/checkout.php
@@ -1,5 +1,4 @@
 <?php
-
 include "../include.php";
 
 $usersid = filterRequest("usersid");
@@ -11,50 +10,41 @@ $couponid = filterRequest("couponid");
 $paymentmethod = filterRequest("paymentmethod");
 $coupondiscount = filterRequest("coupondiscount");
 
-
 if ($orderstype == "1") {
     $pricedelivery = 0;
 }
 
-$totalprice = $ordersprice  + $pricedelivery;
+$totalprice = $ordersprice + $pricedelivery;
 
-
-// Check Coupon 
-
+// Check Coupon
 $now = date("Y-m-d H:i:s");
+$checkcoupon = getData("coupon", "coupon_id = '$couponid' AND coupon_expiredate > '$now' AND coupon_count > 0'", null, false);
 
-$checkcoupon = getData("coupon", "coupon_id = '$couponid' AND coupon_expiredate > '$now' AND coupon_count > 0  ", null,  false);
-
-
-if ($checkcoupon  > 0) {
-    $totalprice =  $totalprice - $ordersprice * $coupondiscount / 100;
-    $stmt = $con->prepare("UPDATE `coupon` SET  `coupon_count`= `coupon_count` - 1  WHERE coupon_id = '$couponid' ");
+if ($checkcoupon > 0) {
+    $totalprice = $totalprice - $ordersprice * $coupondiscount / 100;
+    $stmt = $con->prepare("UPDATE `coupon` SET `coupon_count` = `coupon_count` - 1 WHERE coupon_id = '$couponid'");
     $stmt->execute();
 }
 
-
-
-
 $data = array(
-    "orders_usersid"  =>  $usersid,
-    "orders_address"  =>  $addressid,
-    "orders_type"  =>  $orderstype,
-    "orders_pricedelivery"  =>  $pricedelivery,
-    "orders_price"  =>  $ordersprice,
-    "orders_coupon"  =>  $couponid,
-    "orders_totalprice"  =>  $totalprice,
-    "orders_paymentmethod"  =>  $paymentmethod
+    "orders_usersid" => $usersid,
+    "orders_address" => $addressid,
+    "orders_type" => $orderstype,
+    "orders_pricedelivery" => $pricedelivery,
+    "orders_price" => $ordersprice,
+    "orders_coupon" => $couponid,
+    "orders_totalprice" => $totalprice,
+    "orders_paymentmethod" => $paymentmethod
 );
 
 $count = insertData("orders", $data, false);
 
 if ($count > 0) {
-
-    $stmt = $con->prepare("SELECT MAX(orders_id) from orders ");
+    $stmt = $con->prepare("SELECT MAX(orders_id) FROM orders");
     $stmt->execute();
     $maxid = $stmt->fetchColumn();
 
     $data = array("cart_orders" => $maxid);
-
-    updateData("cart", $data, "cart_usersid = $usersid  AND cart_orders = 0 ");
+    updateData("cart", $data, "cart_usersid = $usersid AND cart_orders = 0");
 }
+
