ebddfe7f2a25dd5deef7189c84f41bbb79db8c94
islamdev96 <islamsayedglab@gmail.com>
2023-07-11 15:31:31 +0300

    ..

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $dsn = "mysql:host=localhost;dbname=u700906284_exfai2";
-$user = "u700906284_islam2";
-$pass = "Islam253**";
+$user = "u700906284_exfai2";
+$pass = "Islam222";
 $option = array(
    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
 );
@@ -14,7 +14,9 @@ try {
    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin");
    header("Access-Control-Allow-Methods: POST, OPTIONS , GET");
    include "functions.php";
+   // if (!isset($notAuth)) {
+   //    checkAuthenticate();
+   // }
 } catch (PDOException $e) {
    echo $e->getMessage();
-
 }
