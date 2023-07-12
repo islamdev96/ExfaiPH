d94b54b840cbba4f7cd4efa15006694f41af4d5d
islamdev96 <islamsayedglab@gmail.com>
2023-07-12 06:32:21 +0300

    ..

--- orders/checkout.php
+++ orders/checkout.php
@@ -1,22 +1,23 @@
 <?php
+
 ini_set("display_errors", "on");
 include "../include.php";
 
-$table = "orders";
+$ordersTable = "orders";
 
 $usersid = filterRequest("userid");
 $addressid = filterRequest("addressid");
 $orderstype = filterRequest("orderstype");
 $ordersprice = filterRequest("ordersprice");
 $deliveryprice = filterRequest("deliveryprice");
-$coupontd = filterRequest("orderscoupon");
+$coupontd = filterRequest("coupontd"); // Updated variable name to match the column name
 $paymentmethod = filterRequest("paymentmethod");
 $coupondiscount = filterRequest("coupondiscount");
 
 $totalprice = $ordersprice + $deliveryprice;
 
 $datenow = date("Y-m-d H:i:s");
-$checkcoupon = getData($table, "coupon_id = $coupontd AND coupon_expiredate > '$datenow' AND coupon_count > 0", null, false);
+$checkcoupon = getData("coupon", "coupon_id = $coupontd AND coupon_expiredate > '$datenow' AND coupon_count > 0", null, false);
 
 try {
     if ($checkcoupon > 0) {
@@ -28,16 +29,17 @@ try {
         "orders_address" => $addressid,
         "orders_type" => $orderstype,
         "orders_price" => $ordersprice,
-        "orders_deliveryprice" => $deliveryprice,
+        "orders_pricedelivery" => $deliveryprice, // Updated column name to match the table structure
         "orders_coupon" => $coupontd,
-        "orders_totalprice" => $totalprice,
-        "orders_paymentmethod" => $paymentmethod
+        "orders_price" => $totalprice, // Updated column name to match the table structure
+        "orders_paymentmethod" => $paymentmethod,
+        "orders_datetime" => $datenow // Added column name to match the table structure
     );
 
-    $count = insertData($table, $data, false);
+    $count = insertData($ordersTable, $data, false);
 
     if ($count > 0) {
-        $statement = $connect->prepare("SELECT MAX(orders_id) FROM orders");
+        $statement = $connect->prepare("SELECT MAX(orders_id) FROM $ordersTable");
         $statement->execute();
         $maxid = $statement->fetchColumn();
 
@@ -50,4 +52,6 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-?>
+
+// Remove the duplicate getData() function from functions.php
+
