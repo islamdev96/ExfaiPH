c7014b4d2dc5aab2cf2cabf19098d3391fb8cf8a
islamdev96 <islamsayedglab@gmail.com>
2023-07-12 07:05:59 +0300

    ..

--- orders/checkout.php
+++ orders/checkout.php
@@ -1,57 +1,49 @@
 <?php
-
 ini_set("display_errors", "on");
-include "../include.php";
+include "../include.php"  ;
 
-$ordersTable = "orders";
+$table = "orders";
 
 $usersid = filterRequest("userid");
 $addressid = filterRequest("addressid");
 $orderstype = filterRequest("orderstype");
 $ordersprice = filterRequest("ordersprice");
 $deliveryprice = filterRequest("deliveryprice");
-$coupontd = filterRequest("coupontd"); // Updated variable name to match the column name
+$couponid = filterRequest("orderscoupon");
 $paymentmethod = filterRequest("paymentmethod");
 $coupondiscount = filterRequest("coupondiscount");
 
 $totalprice = $ordersprice + $deliveryprice;
 
 $datenow = date("Y-m-d H:i:s");
-$checkcoupon = getData("coupon", "coupon_id = $coupontd AND coupon_expiredate > '$datenow' AND coupon_count > 0", null, false);
+$checkcoupon = getData($table, "coupon_id = $couponid AND coupon_expiredate > '$datenow' AND coupon_count > 0", null, false);
 
-try {
-    if ($checkcoupon > 0) {
-        $totalprice = $ordersprice - ($ordersprice * $coupondiscount / 100);
-    }
-
-    $data = array(
-        "orders_usersid" => $usersid,
-        "orders_address" => $addressid,
-        "orders_type" => $orderstype,
-        "orders_price" => $ordersprice,
-        "orders_pricedelivery" => $deliveryprice, // Updated column name to match the table structure
-        "orders_coupon" => $coupontd,
-        "orders_price" => $totalprice, // Updated column name to match the table structure
-        "orders_paymentmethod" => $paymentmethod,
-        "orders_datetime" => $datenow // Added column name to match the table structure
-    );
-
-    $count = insertData($ordersTable, $data, false);
-
-    if ($count > 0) {
-        $statement = $connect->prepare("SELECT MAX(orders_id) FROM $ordersTable");
-        $statement->execute();
-        $maxid = $statement->fetchColumn();
-
-        $data = array(
-            "cart_orders" => $maxid
-        );
-
-        updateData("cart", $data, "cart_usersid = $usersid AND cart_orders = '@'");
-    }
-} catch (Exception $e) {
-    echo $e->getMessage();
+if ($checkcoupon > 0) {
+    $totalprice = $ordersprice - ($ordersprice * $coupondiscount / 100);
 }
 
-// Remove the duplicate getData() function from functions.php
+$data = array(
+    "orders_usersid" => $usersid,
+    "orders_address" => $addressid,
+    "orders_type" => $orderstype,
+    "orders_price" => $ordersprice,
+    "orders_deliveryprice" => $deliveryprice,
+    "orders_coupon" => $couponid,
+    "orders_totalprice" => $totalprice,
+    "orders_paymentmethod" => $paymentmethod
+);
+
+$count = insertData($table, $data, false);
+
+if ($count > 0) {
+    $statement = $connect->prepare("SELECT MAX(orders_id) FROM orders");
+    $statement->execute();
+    $maxid = $statement->fetchColumn();
+
+    $data = array(
+        "cart_orders" => $maxid
+    );
+
+    updateData("cart", $data, "cart_usersid = $usersid AND cart_orders = 0");
+}
 
